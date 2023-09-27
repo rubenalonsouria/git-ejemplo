8fdf37ae3b2f8f1db6b2bcb5c3b82d8fd0761b40
rubenalonsouria <145966835+rubenalonsouria@users.noreply.github.com>
2023-09-27 10:48:56 +0200

    cambio

--- PruebaGit/src/HolaMundo.java
+++ PruebaGit/src/HolaMundo.java
@@ -2,7 +2,7 @@
 public class HolaMundo {
 	
 	public static void main(String[] args) {
-		System.out.println("hola mundo!");
+		System.out.println("hola mundo!"); aaaaaaaa
 	}
 	public int sumae(int a, int b) {
 		return a+b

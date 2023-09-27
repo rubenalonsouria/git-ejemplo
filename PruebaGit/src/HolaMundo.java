e8323ce30b7a4e93c1de1c0e0fe1ce5b1d08c6fb
Ruben Alonso <r.alonso@opendeusto.es>
2023-09-27 10:47:57 +0200

    quitar saludo

--- PruebaGit/src/HolaMundo.java
+++ PruebaGit/src/HolaMundo.java
@@ -2,9 +2,9 @@
 public class HolaMundo {
 	
 	public static void main(String[] args) {
-		System.out.println("hola mundo!");
+		
 	}
-	public int sumae(int a, int b) {
-		return a+b
+	public int sumar(int a, int b) {
+		return a+b;
 	}
 }
